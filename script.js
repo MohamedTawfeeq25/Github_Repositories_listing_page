05c21a9d98f304d08a492cebe894a60bebf4df17
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2024-01-20 15:51:36 +0530

    repo design completed and data are displayed

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+var Repo=(count,repo_name,repo_description,language,url)=>{
+	var parent=document.getElementById("repo-section");
+	
+		for(var i=0;i<count;i++){
+		//main repo div 
+		var repo=document.createElement("div");
+		repo.setAttribute('class','repo');
+			repo.setAttribute('id',repo_name[i]);
+		
+			//reponame div
+			var repoName=document.createElement("div");
+			repoName.setAttribute('id','repo-name');
+				var p=document.createElement("p");
+				p.textContent=repo_name[i];
+			repoName.appendChild(p);
+		
+			//repo description
+			var desc=document.createElement("div");
+			desc.setAttribute('id','description');
+				var p1=document.createElement("p");
+				p1.textContent=repo_description[i];
+			desc.appendChild(p1);
+			
+			//repo language
+			if(language[i]!=null){
+				var lang=document.createElement("div");
+			lang.setAttribute('id','language');
+				var la=document.createElement('div');
+				la.textContent=language[i];
+			lang.appendChild(la);
+			}
+		//appending to main repo
+		repo.appendChild(repoName);
+		repo.appendChild(desc);
+		if(language[i]!=null){
+			repo.appendChild(lang);
+		}
+		parent.appendChild(repo);
+	//appending to parent
+
+		}
+		
+		
+
+}
 var fet=()=>{
 	fetch("https://api.github.com/users/MohamedTawfeeq25")
 	.then(res=>res.json())
@@ -10,10 +55,26 @@ var fet=()=>{
 		document.getElementById("git").href=res.html_url;
 	})
 	.catch((err)=>{console.log(err)});
-
+	var repo_name=[];
+	var repo_description=[];
+	var language=[];
+	var url=[];var count=0;
 	fetch("https://api.github.com/users/MohamedTawfeeq25/repos")
 	.then(res=>res.json())
-	.then(res=>{console.log(res);
+	.then(res=>{
+		count=res.length;
+		for(var i=0;i<res.length;i++){
+			repo_name.push(res[i].name);
+			repo_description.push(res[i].description);
+			url.push(res[i].html_url);
+			language.push(res[i].language);
+		}
+		console.log(repo_name);
+		console.log(repo_description);
+		console.log(language);
+		console.log(url);
+		console.log(count);
+		Repo(count,repo_name,repo_description,language,url);
 	})
 	.catch((err)=>{console.log(err)});
 }
